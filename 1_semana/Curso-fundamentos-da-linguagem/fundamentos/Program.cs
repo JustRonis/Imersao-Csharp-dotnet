12b2e51f48c38e67e53708113b774a711d692ff5
ronaldo <ronaldo.luizcp@gmail.com>
2022-01-02 20:15:23 -0300

    dia 2 - formatacao de numero, operador ternario, notacao ponto, leitura de dados do console

--- 1_semana/Curso-fundamentos-da-linguagem/fundamentos/Program.cs
+++ 1_semana/Curso-fundamentos-da-linguagem/fundamentos/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-using System;
+using System.Globalization;
 
 namespace Fundamentos{
     class Program{
@@ -9,6 +9,39 @@ namespace Fundamentos{
             Console.WriteLine($"Interpolação de string {a}. FIM ");
 
             Console.WriteLine($" 1 + 1 = {1+1}");
+            EhPar();
         }
+
+        static void EhPar(){
+            Console.WriteLine("Digite um número: ");
+            var numero = Console.Read();
+            if(numero % 2 == 0){
+                Console.WriteLine("O número é par");
+            }else{
+                Console.WriteLine("O número é impar");
+            }
+
+            EhPar2(6);
+        }
+
+        static void EhPar2(int n1){
+            string resposta = n1 % 2 == 0 ? "O número é par" : "o número é impar";
+            Console.WriteLine(resposta);
+
+            FormatandoNumeros();
+        }
+
+        static void FormatandoNumeros(){
+            double valor = 15.175;
+            Console.WriteLine(valor.ToString("F1")); // formatar para apenas uma casa decimal
+            Console.WriteLine(valor.ToString("C")); // formatar para 'coin' | moeda 
+            Console.WriteLine(valor.ToString("P")); // formatar para percentual
+            Console.WriteLine(valor.ToString("#.##")); // formatar para duas casas decimais, seria o mesmo que 'valor.ToString("F2");
+
+            var cultura =  new CultureInfo("pt-BR");
+             Console.WriteLine(valor.ToString("C", cultura)); // formatar para 'coin' | moeda  - usando o padrão de moeda brasileiro
+            
+        }
+
     }
 }
