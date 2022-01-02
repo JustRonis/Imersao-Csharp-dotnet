995ee6b7285427291168b7fb89ed0aa372f715bb
ronaldo <ronaldo.luizcp@gmail.com>
2022-01-01 21:36:20 -0300

    Commitando dia 1

---
+++ 1_semana/Curso-fundamentos-da-linguagem/fundamentos/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+namespace Fundamentos{
+    class Program{
+        static void Main(string [] args){
+            var a = "EU VIM DE UMA VARIAVEl";
+            Console.WriteLine($"Interpolação de string {a}. FIM ");
+
+            Console.WriteLine($" 1 + 1 = {1+1}");
+        }
+    }
+}
