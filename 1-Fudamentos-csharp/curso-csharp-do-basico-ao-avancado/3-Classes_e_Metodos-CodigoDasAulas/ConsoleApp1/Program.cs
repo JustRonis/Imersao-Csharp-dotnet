0de4d752932c7e1b563c7e6849bf517af27b7778
Ronis <ronaldo.luizcp@gmail.com>
2022-02-23 21:06:51 -0300

    Subindo arquivos do modulo Classes e métodos, e também a teoria do que é uma classe

---
+++ 1-Fudamentos-csharp/curso-csharp-do-basico-ao-avancado/3-Classes_e_Metodos-CodigoDasAulas/ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿/* --------------------------- TEORIA ----------------------------------------
+O que é uma classe?
+R: 
+Classe é um tipo, tal como um int, uma string, um bool...
+Classe define um tipo de uma estrutura de dado
+Se eu crio uma classe "pessoa", por exemplo. Já que classe é um tipo, significa que posso criar uma variável do tipo pessoa. Sim!
+
+Exemplo de uma estrura de classe:
+
+Class Pessoa {
+    [atributos = dados]
+    [metodos = algoritimos]
+    Tanto os atributos quanto os metodos são os membros da classe Pessoa.
+}
+
+O que é um objeto?
+R: Ao instanciar uma classe, um objeto é criado. 
+
+Qual a difirença entre uma classe e um objeto?
+R:
+*/
+
+
+
+
+using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
