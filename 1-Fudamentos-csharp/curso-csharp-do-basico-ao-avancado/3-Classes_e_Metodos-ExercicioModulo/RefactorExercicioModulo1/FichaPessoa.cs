0d2b2fc2a2d3ed2061a2862a863abc0a63cdeac0
Ronis <ronaldo.luizcp@gmail.com>
2022-03-02 11:13:18 -0300

    Removendo comentários desnecessários

--- 1-Fudamentos-csharp/curso-csharp-do-basico-ao-avancado/3-Classes_e_Metodos-ExercicioModulo/RefactorExercicioModulo1/FichaPessoa.cs
+++ 1-Fudamentos-csharp/curso-csharp-do-basico-ao-avancado/3-Classes_e_Metodos-ExercicioModulo/RefactorExercicioModulo1/FichaPessoa.cs
@@ -18,29 +18,6 @@ namespace RefactorExercicioModulo1
         public string Rg { get; set; }
         public string Cpf { get; set; }
    
-        //public FichaPessoa(int id, string nome, int idade, string sexo, string nomeMae, string nomePai, string rg, string cpf)
-        //{
-        //    if(id < 0)
-        //        throw new InvalidOperationException();
-
-        //    if(idade < 0)
-        //        throw new InvalidOperationException();
-
-        //    if (string.IsNullOrEmpty(nome) || string.IsNullOrEmpty(sexo) || string.IsNullOrEmpty(nomeMae) || string.IsNullOrEmpty(nomePai))
-        //    {
-        //        throw new InvalidOperationException();
-        //    }
-
-        //    if (!Regex.IsMatch(rg, @"(^\d{1,2}).?(\d{3}).?(\d{3})-?(\d{1}|X|x$)"))
-        //    {
-        //        throw new InvalidOperationException();
-        //    }
-
-        //    if (!Regex.IsMatch(cpf, @"(^\d{3}\.\d{3}\.\d{3}\-\d{2}$)"))
-        //    {
-        //        throw new InvalidOperationException();
-        //    }
-        //}
 
     }
 
