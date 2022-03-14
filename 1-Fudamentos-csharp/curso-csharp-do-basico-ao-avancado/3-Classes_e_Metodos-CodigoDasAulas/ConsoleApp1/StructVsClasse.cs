323191ea35aaa24bda18f058e31030cf239e1f65
Ronis <ronaldo.luizcp@gmail.com>
2022-03-14 12:59:20 -0300

    structr

--- 1-Fudamentos-csharp/curso-csharp-do-basico-ao-avancado/3-Classes_e_Metodos-CodigoDasAulas/ConsoleApp1/StructVsClasse.cs
+++ 1-Fudamentos-csharp/curso-csharp-do-basico-ao-avancado/3-Classes_e_Metodos-CodigoDasAulas/ConsoleApp1/StructVsClasse.cs
@@ -21,16 +21,16 @@ namespace ConsoleApp1
         }
         public static void Executar()
         {
-            SPonto ponto1 = new SPonto { X = 1, Y = 3 };
-            SPonto copiaPonto1 = ponto1;
+            SPonto ponto1 = new SPonto { X = 1, Y = 3 }; //STRUCT
+            SPonto copiaPonto1 = ponto1; //Atribuição por valor
             ponto1.X = 3;
 
             Console.WriteLine("Ponto 1 X:{0}", ponto1.X);
             Console.WriteLine("Copia Ponto 1 X:{0}", copiaPonto1.X);
 
-            CPonto ponto2 = new CPonto { X = 1, Y = 3 };
+            CPonto ponto2 = new CPonto { X = 1, Y = 3 }; //CLASSE
             CPonto copiaPonto2 = ponto2;
-            ponto2.X = 4;
+            ponto2.X = 4; //Atribuição por refencia
             Console.WriteLine("Ponto 2 X:{0}", ponto2.X);
             Console.WriteLine("Copia Ponto 2 X:{0}", copiaPonto2.X);
 
