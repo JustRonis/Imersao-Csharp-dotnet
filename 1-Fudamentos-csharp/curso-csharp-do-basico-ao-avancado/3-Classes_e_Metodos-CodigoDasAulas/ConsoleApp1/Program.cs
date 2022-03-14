a517acbaa8f4f2ccc07b5441532b4e3a32b372ff
Ronis <ronaldo.luizcp@gmail.com>
2022-03-14 11:01:02 -0300

    subindo atributos

--- 1-Fudamentos-csharp/curso-csharp-do-basico-ao-avancado/3-Classes_e_Metodos-CodigoDasAulas/ConsoleApp1/Program.cs
+++ 1-Fudamentos-csharp/curso-csharp-do-basico-ao-avancado/3-Classes_e_Metodos-CodigoDasAulas/ConsoleApp1/Program.cs
@@ -62,6 +62,8 @@ namespace ConsoleApp1
             Params.Recepcionar("Ronaldo", "Luiza", "Lidia", "Talu");
 
             GetSet.Executar();
+
+            Props.Executar();
         }
     }
 }
--- 1-Fudamentos-csharp/curso-csharp-do-basico-ao-avancado/3-Classes_e_Metodos-CodigoDasAulas/ConsoleApp1/Props.cs
+++ 1-Fudamentos-csharp/curso-csharp-do-basico-ao-avancado/3-Classes_e_Metodos-CodigoDasAulas/ConsoleApp1/Props.cs
@@ -7,11 +7,50 @@ using System.Threading.Tasks;
 namespace ConsoleApp1
 {
     public class CarroOpcional
+    {
+        private double desconto = 0.1;
+        private string nome;
+        public string Nome
+        {
+            get
+            {
+                return "Opcional: " + nome;
+            }
+            set
+            {
+                nome = value;
+            }
+        }
+
+        //propriedade autoimplementada
+        public double Preco { get; set; }
+
+        // somente leitura
+        public double PrecoComDesconto
+        {
+            get => Preco - (desconto * Preco); //Lambda
+        }
+
+        public CarroOpcional() { }
+
+        public CarroOpcional(string nome, double preco)
+        {
+            // this.nome = nome;
+            Nome = nome;
+            Preco = preco;
+        }
+    }
     class Props
     {
 
         public static void Executar()
         {
+            var op1 = new CarroOpcional("Ar condicionado", 3500);
+            Console.WriteLine(op1.PrecoComDesconto);
+
+            var op2 = new CarroOpcional("Direção Eletrica", 2500);
+            Console.WriteLine(op2.Nome);
+
 
         }
     }
